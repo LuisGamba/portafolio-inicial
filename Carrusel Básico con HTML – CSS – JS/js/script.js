f2f03d578bfe6fc213b13e66218987c3411370f9
LuisGamba <luisgamto@gmail.com>
2023-05-23 09:44:51 -0500

    carrusel

--- Carrusel Básico con HTML – CSS – JS/js/script.js
+++ Carrusel Básico con HTML – CSS – JS/js/script.js
@@ -3,12 +3,12 @@ const btnRight = document.querySelector(".btn-right");
 const botones = document.querySelector(".btn");
 const imagenes = document.querySelector(".images");
 
-let imgMuestra = ["001","002","003","004"];
+let imgMuestra = ["001", "002", "003", "004"];
 
 let contador = 0;
 
-btnLeft.addEventListener("click",izquierda);
-btnRight.addEventListener("click",derecha);
+btnLeft.addEventListener("click", izquierda);
+btnRight.addEventListener("click", derecha);
 
 function derecha(){
     contador++;
@@ -21,7 +21,7 @@ function derecha(){
 function izquierda(){
     contador--;
     if(contador<0){
-        contador = imgMuestra.lenght-1;
+        contador = imgMuestra.length-1;
     }
     imagenes.style.background = `url("img/${imgMuestra[contador]}.jpg")`;
 }
